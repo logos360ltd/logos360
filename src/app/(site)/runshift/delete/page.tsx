6b9a99a605d346a163bf02f8561893ad6f5111e7
itsabubakar <seeker99157@gmail.com>
2024-07-19 15:50:19 +0100

    added delete accont route

---
+++ src/app/(site)/runshift/delete/page.tsx
@@ -0,0 +1,26 @@
+import Breadcrumb from "@/components/Common/Breadcrumb"
+import ScrollUp from "@/components/Common/ScrollUp"
+import { Metadata } from "next";
+
+type Props = {}
+
+export const metadata: Metadata = {
+  title:
+    " | Logos360 - Innovating Digital Solutions",
+};
+
+const page = (props: Props) => {
+  return (
+    <div>
+       <ScrollUp />
+
+      <Breadcrumb pageName="Delete Account" />
+      <div className="max-w-5xl mx-auto px-5 lg:px-0 my-10">
+        <p>Please email admin@logos360.co.uk with details of what you want deleted and we will get back to you withn 24 hours</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default page
